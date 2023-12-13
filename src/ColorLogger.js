2eb04931559cf48c4e452b4c4a151e43181382a2
athomas227 <andrewthomas@pursuit.org>
2023-12-13 18:12:11 -0500

    finished exercise

---
+++ src/ColorLogger.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+function ColorLogger() {
+    const [color, setColor] = useState('black');
+
+    useEffect(() => {
+        console.log('color changed to: ${color}');
+    }, [color]);
+
+    const colorChange = (newColor) => {
+        setColor(newColor);
+    }
+
+    return (
+        <div>
+          <h2 style={{ color: color }}>Color</h2>
+          
+          <button onClick={() => colorChange('red')}>Set Red</button>
+          <button onClick={() => colorChange('blue')}>Set Blue</button>
+          <button onClick={() => colorChange('green')}>Set Green</button>
+        </div>
+      );
+}
+
+export default ColorLogger;
